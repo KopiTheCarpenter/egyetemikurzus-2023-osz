20fbb6af6fa8e7077b91182c19fe66dbe9d7ab44
Ács Koppány <acs.koppany.copy@gmail.com>
2023-12-08 12:21:22 +0100

    Added Henger model

---
+++ cke7xe/Model/Henger.cs
@@ -0,0 +1,9 @@
+﻿namespace cke7xe.Model
+{
+    public record Henger
+    {
+        public int Id { get; set; }
+        public string Megnevezes {  get; set; }
+        public int Atmero { get; set; }
+    }
+}
